6472c5b63f4bcc3b8d192563545fede1333849b5
volkan <volkanbsky@gmail.com>
2020-07-22 19:54:11 +0300

    fef

--- Stock-Service/src/main/java/ros/repositorys/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package ros.repositorys;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import ros.models.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
